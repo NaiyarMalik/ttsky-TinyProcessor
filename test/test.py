b205ecf36cd911f4619d80d9ab3a23c696094a8c
NaiyarMalik <naiyar2006@gmail.com>
2026-07-29 21:34:19 -0400

    New Test.py

--- test/test.py
+++ test/test.py
@@ -1,40 +1,62 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import ClockCycles, Timer
+
+
+async def uart_send_bit(dut, bit):
+    dut.ui_in.value = bit
+    await ClockCycles(dut.clk, 32*14)
+
+
+async def uart_send_byte(dut, data):
+
+    # start bit
+    await uart_send_bit(dut, 0)
+
+    # data bits LSB first
+    for i in range(8):
+        await uart_send_bit(dut, (data >> i) & 1)
+
+    # stop bit
+    await uart_send_bit(dut, 1)
 
 
 @cocotb.test()
 async def test_project(dut):
+
     dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
+    # 50 MHz clock
+    clock = Clock(dut.clk, 20, unit="ns")
     cocotb.start_soon(clock.start())
 
+
     # Reset
-    dut._log.info("Reset")
     dut.ena.value = 1
-    dut.ui_in.value = 0
+    dut.ui_in.value = 1
     dut.uio_in.value = 0
+
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
+
     dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 100)
 
-    dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    dut._log.info("Sending WRITE command")
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    # WRITE command
+    # AA = write opcode
+    # address = 5
+    # data = FF
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    await uart_send_byte(dut, 0xAA)
+    await uart_send_byte(dut, 0x05)
+    await uart_send_byte(dut, 0xFF)
+
+
+    await Timer(10, units="us")
+
+
+    dut._log.info("Test complete")
